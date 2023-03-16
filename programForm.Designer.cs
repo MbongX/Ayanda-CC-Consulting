efc5bae24a8400ff9a8d58cc3b94f56e93273bdb
MbongX <mbongenimahlangu203@outlook.com>
2023-03-17 01:29:45 +0200

    Workshop Complete

--- programForm.Designer.cs
+++ programForm.Designer.cs
@@ -32,51 +32,75 @@
             this.lblDate = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lblProgDetails = new System.Windows.Forms.Label();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(27, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 19);
+            this.label1.Size = new System.Drawing.Size(41, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Date:";
             // 
             // lblDate
             // 
             this.lblDate.AutoSize = true;
-            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDate.Location = new System.Drawing.Point(147, 34);
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDate.Location = new System.Drawing.Point(130, 34);
             this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(0, 19);
+            this.lblDate.Size = new System.Drawing.Size(0, 17);
             this.lblDate.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.Location = new System.Drawing.Point(27, 104);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 19);
+            this.label3.Size = new System.Drawing.Size(90, 17);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Programmer:";
             // 
             // lblProgDetails
             // 
             this.lblProgDetails.AutoSize = true;
-            this.lblProgDetails.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblProgDetails.Location = new System.Drawing.Point(147, 104);
+            this.lblProgDetails.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblProgDetails.Location = new System.Drawing.Point(130, 104);
             this.lblProgDetails.Name = "lblProgDetails";
-            this.lblProgDetails.Size = new System.Drawing.Size(0, 19);
+            this.lblProgDetails.Size = new System.Drawing.Size(0, 17);
             this.lblProgDetails.TabIndex = 3;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(206, 179);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(148, 34);
+            this.btnView.TabIndex = 4;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(411, 179);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(148, 34);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // programmerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1268, 208);
+            this.ClientSize = new System.Drawing.Size(751, 241);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnView);
             this.Controls.Add(this.lblProgDetails);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblDate);
@@ -95,5 +119,7 @@
         private Label lblDate;
         private Label label3;
         private Label lblProgDetails;
+        private Button btnView;
+        private Button btnBack;
     }
 }
--- programForm.cs
+++ programForm.cs
@@ -19,7 +19,7 @@ namespace Ayanda_CC_Consulting
 
         private void programForm_Load(object sender, EventArgs e)
         {
-            DateTime date = DateTime.Today;
+            DateTime date = DateTime.Now;
             lblDate.Text = date.ToString();
             //string name = "Nobesuthu Sithole"; 
            // string stdID = "STHNOB004"; 
@@ -27,7 +27,18 @@ namespace Ayanda_CC_Consulting
            // string institution = "CPUT";
 
 
-            lblProgDetails.Text = "The programmer is Nobesuthu Sithole, stdID STHNOB004, doing her Bachelor of Science majoring in Applied Statistics and information systems at CPUT";
+            lblProgDetails.Text = "The programmer is Nobesuthu Sithole with Student ID: STHNOB004 from CPUT";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            programForm_ pg = new programForm_();
+            pg.ShowDialog();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
